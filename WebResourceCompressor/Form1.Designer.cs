41033ef08f1498baa12b22bc043ca49222cb121d
marahaman <sun2moon@gmail.com>
2013-03-04 15:34:31 +1030

    Commiting the project

---
+++ WebResourceCompressor/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WebResourceCompressor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(625, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select the css/js Directory";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(556, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(278, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.textBox2.Location = new System.Drawing.Point(12, 85);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(863, 256);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 353);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "CSS/JS Minifier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+
+    }
+}
+
---
+++ WebResourceCompressor/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using Yahoo.Yui.Compressor;
+
+namespace WebResourceCompressor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = folderBrowserDialog1.SelectedPath;
+                this.textBox1.Enabled = false;
+                label1.Text = "Compressing. Please wait for a while";
+                searchDirectory(this.textBox1.Text);
+                label1.ForeColor = Color.Green;
+                label1.Text = "Successfully compressed the css and js files; Enjoy :)";
+            }
+        }
+
+        private void searchDirectory(string sDir)
+        {
+            try
+            {
+                if (Directory.GetDirectories(sDir, "*.*", SearchOption.AllDirectories).Length > 0)
+                {
+                    foreach (string d in Directory.GetDirectories(sDir,"*.*",SearchOption.AllDirectories ))
+                    {
+                        foreach (string f in Directory.GetFiles(d))
+                        {
+
+                            compressFile(f);
+                            textBox2.Text += f+"\r\n";
+                            //Console.WriteLine(f);
+                        }
+                        //DirSearch(d);
+                    }
+
+
+
+                }
+                //else
+                //{
+                    foreach (string f in Directory.GetFiles(sDir))
+                    {
+                        compressFile(f);
+                        textBox2.Text += f + "\r\n";
+                        //Console.WriteLine(f);
+                    }
+                //}
+
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        void compressFile(string f)
+        {
+            try
+            {
+                var thefile = File.ReadAllText(f);
+
+                if (Path.GetExtension(f) == ".js")
+                {
+                    var jsCompressor = new JavaScriptCompressor();
+                    var mini = jsCompressor.Compress(thefile);
+                    if (mini.ToString() != null)
+                        File.WriteAllText(f, mini.ToString());
+                }
+                else if (Path.GetExtension(f) == ".css")
+                {
+                    var cssCompressor = new CssCompressor();
+                    var mini = cssCompressor.Compress(thefile);
+                    if (mini.ToString() != null)
+                        File.WriteAllText(f, mini.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+
+        }
+    }
+}
